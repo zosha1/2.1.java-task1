e6598dfa9152c59075d71b57e7bc80f0f040ee22
Zosha <posyamyka@gmail.com>
2021-05-15 16:50:40 +0300

    mailes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int coefficient = 20;
+        double ticket  = 14500.70;
+        int bonus = (int) ticket / coefficient;
+        System.out.println(bonus);
+    }
+}
